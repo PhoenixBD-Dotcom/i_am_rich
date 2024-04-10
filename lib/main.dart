fb3befb506a401f9cf2b11f2ff0281fcde5761ed
Md.Sakibullah <sakibullah98@gmail.com>
2024-04-11 01:33:26 +0600

    adding scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(
-        child: Image(
-          image: AssetImage('assets/diamond.png'), // Load image from asset
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
         ),
+        body: Center(
+            child: Image(
+                image: NetworkImage(
+                    'https://st.depositphotos.com/1015682/2086/i/450/depositphotos_20867981-stock-photo-earth-at-night-with-city.jpg'))),
+        backgroundColor: Colors.blueGrey[500],
       ),
     ),
   );
